aa0a78282b9a88d12e0648f51fffd6145198d644
kelzo8 <kelzoj25@gmail.com>
2024-07-02 12:29:18 +0100

    add titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -16,5 +17,17 @@ with col2:
     st.info(content)
 
 content2 = """
-Below you can see some of my projects which I have worked on both by myself and in a team"""
+Below you can see some of my projects which I have worked on both by myself and in a team. Feel free to contact me!"""
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
