e38e0aa3eeba71edededaafdd615bf198eb507ed
kelzo8 <kelzoj25@gmail.com>
2024-07-02 10:42:20 +0100

    add text

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image("images/photo.png", width=300)
 
@@ -13,3 +14,7 @@ with col2:
     Hi I am a software engineering student
     """
     st.info(content)
+
+content2 = """
+Below you can see some of my projects which I have worked on both by myself and in a team"""
+st.write(content2)
