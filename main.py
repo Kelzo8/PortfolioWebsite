03e75dc7172c983c9583292e9ba2609f7e110c2d
kelzo8 <kelzoj25@gmail.com>
2024-07-02 12:41:35 +0100

    add links, descriptions, link

--- main.py
+++ main.py
@@ -20,14 +20,20 @@ content2 = """
 Below you can see some of my projects which I have worked on both by myself and in a team. Feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
 
